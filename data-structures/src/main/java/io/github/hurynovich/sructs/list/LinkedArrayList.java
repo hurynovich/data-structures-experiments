2cdd87dd250b019ce63d2f732ebb191dab137e16
Pavel Hurynovich <pavel.gurinovich@gmail.com>
2020-02-18 16:37:53 +0300

    added remark

--- data-structures/src/main/java/io/github/hurynovich/sructs/list/LinkedArrayList.java
+++ data-structures/src/main/java/io/github/hurynovich/sructs/list/LinkedArrayList.java
@@ -5,6 +5,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
+//TODO investigate other implementation https://github.com/lecousin/java-framework-core/blob/master/net.lecousin.core/src/main/java/net/lecousin/framework/collections/LinkedArrayList.java
 public final class LinkedArrayList extends AbstractList {
-
 }
