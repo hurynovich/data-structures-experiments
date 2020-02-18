e4cec105f236b51347aa2fc65c9f7ed44c06f22e
Pavel Hurynovich <pavel.gurinovich@gmail.com>
2020-02-18 16:25:15 +0300

    added templates for different List implementations

---
+++ data-structures/src/main/java/io/github/hurynovich/sructs/list/LinkedList.java
@@ -0,0 +1,10 @@
+package io.github.hurynovich.sructs.list;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public final class LinkedList extends AbstractList {
+
+}
